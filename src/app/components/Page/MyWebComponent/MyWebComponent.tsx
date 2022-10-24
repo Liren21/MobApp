632e6c3f47341eac897953b67b8814f763f59afa
Liren21 <kuminovvv21@gmail.com>
2022-10-24 22:29:14 +0600

    UPD

---
+++ src/app/components/Page/MyWebComponent/MyWebComponent.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import {observer} from "mobx-react";
+import WebView from "react-native-webview";
+
+import {ActivityIndicator, BackHandler, StatusBar, StyleSheet, View} from "react-native";
+import {useEffect, useRef, useState} from "react";
+
+
+export default observer(() => {
+    const webViewRef = useRef(null)
+    const [canGoBack, setCanGoBack] = useState(false)
+    const [canGoForward, setCanGoForward] = useState(false)
+    const [currentUrl, setCurrentUrl] = useState('https://liren21.github.io/#/')
+
+    const backActive = () => {
+        if (canGoBack) {
+            // @ts-ignore
+            webViewRef.current.goBack()
+        }
+        return true
+    }
+
+    useEffect(() => {
+        BackHandler.addEventListener('hardwareBackPress', backActive);
+        () => BackHandler.removeEventListener('hardwareBackPress', backActive)
+    }, [canGoBack])
+
+    const Loading = () => <ActivityIndicator
+        style={[styles.container, styles.horizontal]}
+        color={"blue"}
+        size={'large'}
+    />
+    return (
+       <View style={{flex:1, width:"100%",height:"100%"}}>
+           <StatusBar/>
+           <WebView
+               ref={webViewRef}
+               source={{uri: currentUrl}}
+               startInLoadingState
+               renderLoading={Loading}
+               onNavigationStateChange={
+                   navState => {
+                       setCanGoBack(navState.canGoBack)
+                       setCanGoForward(navState.canGoForward)
+                       setCurrentUrl(navState.url)
+                   }
+               }
+           />
+       </View>
+    );
+})
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center"
+    },
+    horizontal: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        padding: 10
+    }
+});
--- src/app/components/Router/Route.tsx
+++ src/app/components/Router/Route.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import CardInfo from "../Page/Cards/CardInfo";
 import {createStackNavigator} from "@react-navigation/stack";
 import {NavigationContainer} from "@react-navigation/native";
-import DocSog from "../Page/Doc/DocSog";
 import {observer} from "mobx-react";
-import MyWebComponent from "../general/MyWebComponent";
+import MyWebComponent from "../Page/MyWebComponent/MyWebComponent";
+
 
 
 const Stack = createStackNavigator();
@@ -15,21 +14,11 @@ export default observer(() => {
             <Stack.Navigator>
                 <Stack.Screen name={'Займ'} options={{
                     title: '',
-                    gestureEnabled: true,
+                    gestureEnabled: false,
                     headerStyle: {
-                        backgroundColor: 'rgb(217,214,254)',
-                    }, headerShown: true
-                }} component={CardInfo}/>
-                <Stack.Screen name={'web'} options={{
-                    gestureEnabled: true,
-                    title: '',
-                    headerShown: true
+                        backgroundColor: 'rgba(217,214,254,0)',
+                    }, headerShown: false
                 }} component={MyWebComponent}/>
-                <Stack.Screen name={'Политика конфидициальности'} options={{
-                    gestureEnabled: true,
-                    title: '',
-                    headerShown: false
-                }} component={DocSog}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
